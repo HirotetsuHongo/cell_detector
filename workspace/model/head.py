3d1c35e56dfd1e9a5387e1909ece73c76b22ae44
HirotetsuHongo <hongo.hirotetsu@j.mbox.nagoya-u.ac.jp>
2020-11-06 16:02:09 +0900

    minor changes.

--- workspace/model/head.py
+++ workspace/model/head.py
@@ -5,16 +5,25 @@ from . util import Convolutional, Upsample
 
 
 class YOLOv3(nn.Module):
-    def __init__(self, filters, classes):
+    def __init__(self, num_filters, num_classes,
+                 anchors=[(10,13),
+                          (16,30),
+                          (33,23),
+                          (30,61),
+                          (62,45),
+                          (59,119),
+                          (116,90),
+                          (156,198),
+                          (373,326)]):
         super(YOLOv3, self).__init__()
-        self.darknet = Darknet(filters)
+        self.darknet = Darknet(num_filters)
         self.upsample1 = nn.ModuleList([Convolutional(512, 128, 1),
                                         Upsample()])
         self.upsample2 = nn.ModuleList([Convolutional(1024, 256, 1),
                                         Upsample()])
-        self.scale1 = Convolutional(256, 3 * (4 + 1 + classes), 1)
-        self.scale2 = Convolutional(512, 3 * (4 + 1 + classes), 1)
-        self.scale3 = Convolutional(1024, 3 * (4 + 1 + classes), 1)
+        self.scale1 = Convolutional(256, 3 * (4 + 1 + num_classes), 1)
+        self.scale2 = Convolutional(512, 3 * (4 + 1 + num_classes), 1)
+        self.scale3 = Convolutional(1024, 3 * (4 + 1 + num_classes), 1)
         self.convs1 = nn.ModuleList([Convolutional(384, 128, 1),
                                      Convolutional(128, 256, 3),
                                      Convolutional(256, 128, 1),
