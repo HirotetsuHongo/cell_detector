631ebde0ebabd8ba5f1f69acb15b3b06027be6f5
HirotetsuHongo <hongo.hirotetsu@j.mbox.nagoya-u.ac.jp>
2020-11-21 21:00:38 +0900

    Restructuring head.

--- workspace/model/head.py
+++ workspace/model/head.py
@@ -1,3 +1,4 @@
+import time
 import torch
 import torch.nn as nn
 from . backbone import Darknet
@@ -35,8 +36,12 @@ class YOLOv3(nn.Module):
                                      Convolutional(512, 1024, 3)])
 
     def forward(self, x):
+        t0 = time.time()
         scale1, scale2, x = self.darknet(x)
+        t = time.time()
+        print('Darknet: {:.5f}'.format(t-t0))
 
+        t0 = time.time()
         # Scale3
         for layer in self.convs3:
             x = layer(x)
@@ -62,31 +67,86 @@ class YOLOv3(nn.Module):
             x = layer(x)
 
         scale1 = self.scale1(x)
+        t = time.time()
+        print('YOLOv3 expect Darknet: {:.5f}'.format(t-t0))
 
         return scale1, scale2, scale3
 
 
 class Net(nn.Module):
-    def __init__(self, num_filters, num_classes, confidency):
+    def __init__(self, input_height, input_width, num_filters,
+                 num_classes, confidency, CUDA=True):
         super(Net, self).__init__()
         self.yolov3 = YOLOv3(num_filters, num_classes)
+        self.input_height = input_height
+        self.input_width = input_width
         self.confidency = confidency
+        self.CUDA = CUDA
 
     def forward(self, x):
         # apply YOLOv3
+        t0 = time.time()
         x = self.yolov3(x)
+        t1 = time.time()
+        print('YOLOv3: {:.5f} sec'.format(t1-t0))
 
-        # reshape feature maps
-        x = [x.transpose(1, 3)
-              .reshape(x.size(0),
-                       x.size(3) * x.size(2) * 3,
-                       x.size(1) // 3)
-             for x in x]
+        # transform feature maps
+        t0 = time.time()
+        for xi in x:
+            # define variables
+            batch_size = xi[0]
+            height = xi[1]
+            width = xi[2]
+            num_anchors = 3
+            bbox_size = xi[3] // num_anchors
 
-        # concatnate feature maps into single feature map
-        x = torch.cat(x, 1)
+            # reshape the feature map
+            t0 = time.time()
+            xi = xi.transpose(1, 3)
+            xi = xi.reshape(batch_size, height*width*num_anchors, bbox_size)
+            t1 = time.time()
+            print('reshape: {:.5f} sec'.format(t1-t0))
 
-        # fill cells under confidency with zero
-        # x = x[torch.all(x[:, :, 4] > self.confidency, axis=1)]
+            # apply sigmoid to the confidency
+            # and classes probability distribution
+            t0 = time.time()
+            xi[:, :, 4:] = torch.sigmoid(xi[:, :, 4:])
+            t1 = time.time()
+            print('sigmoid confidency and probability: {:.5f} sec'
+                  .format(t1-t0))
+
+            # apply sigmoid to the tx and ty
+            t0 = time.time()
+            xi[:, :, 0] = torch.sigmoid(xi[:, :, 0])
+            xi[:, :, 1] = torch.sigmoid(xi[:, :, 1])
+            t1 = time.time()
+            print('sigmoid tx and ty: {:.5f} sec'.format(t1-t0))
+
+        # t1 = time.time()
+        # print('transform: {:.5f} sec'.format(t1-t0))
+        # # concatnate feature maps into single feature map
+        # t0 = time.time()
+        # x = torch.cat(x, 1)
+        # t1 = time.time()
+        # print('concatenate: {:.5f} sec'.format(t1-t0))
+
+        # # add cx and cy to tx and ty
+        # t0 = time.time()
+        # cx = torch.arange(x.size(2))
+        # cy = torch.arange(self.input_height)
+        # if self.CUDA:
+        #     cx = cx.cuda()
+        #     cy = cy.cuda()
+        # cx, cy = torch.meshgrid(cx, cy)
+        # cx = cx.reshape(-1, 1)
+        # cy = cy.reshape(-1, 1)
+        # cxy = torch.cat((cx, cy), 1).repeat(1, 3).reshape(-1, 2).unsqueeze(0)
+        # x = x, cxy
+        # t1 = time.time()
+        # print('add cx and cy: {:.5f} sec'.format(t1-t0))
 
         return x
+
+
+x = torch.randn(6, 1, 416, 416).cuda()
+f = Net(416, 416, 1, 4, 0.5).cuda()
