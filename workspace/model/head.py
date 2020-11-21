f1dcc7c15a493c7ec4db12fece6ca7384417d441
HirotetsuHongo <hongo.hirotetsu@j.mbox.nagoya-u.ac.jp>
2020-11-22 03:20:45 +0900

    Restructured.

--- workspace/model/head.py
+++ workspace/model/head.py
@@ -87,63 +87,76 @@ class Net(nn.Module):
         # apply YOLOv3
         t0 = time.time()
         x = self.yolov3(x)
+        x = list(x)
         t1 = time.time()
         print('YOLOv3: {:.5f} sec'.format(t1-t0))
 
         # transform feature maps
-        t0 = time.time()
-        for xi in x:
+        tt0 = time.time()
+        for i in range(len(x)):
             # define variables
-            batch_size = xi[0]
-            height = xi[1]
-            width = xi[2]
+            batch_size = x[i].size(0)
+            height = x[i].size(2)
+            width = x[i].size(3)
             num_anchors = 3
-            bbox_size = xi[3] // num_anchors
+            bbox_size = x[i].size(1) // num_anchors
 
             # reshape the feature map
             t0 = time.time()
-            xi = xi.transpose(1, 3)
-            xi = xi.reshape(batch_size, height*width*num_anchors, bbox_size)
+            x[i] = x[i].transpose(1, 3)
+            x[i] = x[i].reshape(batch_size,
+                                height*width*num_anchors,
+                                bbox_size)
             t1 = time.time()
             print('reshape: {:.5f} sec'.format(t1-t0))
 
             # apply sigmoid to the confidency
             # and classes probability distribution
             t0 = time.time()
-            xi[:, :, 4:] = torch.sigmoid(xi[:, :, 4:])
+            x[i][:, :, 4:] = torch.sigmoid(x[i][:, :, 4:])
             t1 = time.time()
             print('sigmoid confidency and probability: {:.5f} sec'
                   .format(t1-t0))
 
             # apply sigmoid to the tx and ty
             t0 = time.time()
-            xi[:, :, 0] = torch.sigmoid(xi[:, :, 0])
-            xi[:, :, 1] = torch.sigmoid(xi[:, :, 1])
+            x[i][:, :, 0] = torch.sigmoid(x[i][:, :, 0])
+            x[i][:, :, 1] = torch.sigmoid(x[i][:, :, 1])
             t1 = time.time()
             print('sigmoid tx and ty: {:.5f} sec'.format(t1-t0))
 
-        # t1 = time.time()
-        # print('transform: {:.5f} sec'.format(t1-t0))
-        # # concatnate feature maps into single feature map
-        # t0 = time.time()
-        # x = torch.cat(x, 1)
-        # t1 = time.time()
-        # print('concatenate: {:.5f} sec'.format(t1-t0))
+            # add cx and cy to tx and ty
+            t0 = time.time()
+            cx = torch.arange(width)
+            cy = torch.arange(height)
+            if self.CUDA:
+                cx = cx.cuda()
+                cy = cy.cuda()
+            cx, cy = torch.meshgrid(cx, cy)
+            cx = cx.reshape(-1, 1)
+            cy = cy.reshape(-1, 1)
+            cxy = torch.cat((cx, cy), 1) \
+                       .repeat(1, num_anchors) \
+                       .reshape(-1, 2) \
+                       .unsqueeze(0)
+            x[i][:, :, :2] += cxy
+            t1 = time.time()
+            print('add cx and cy: {:.5f} sec'.format(t1-t0))
 
-        # # add cx and cy to tx and ty
-        # t0 = time.time()
-        # cx = torch.arange(x.size(2))
-        # cy = torch.arange(self.input_height)
-        # if self.CUDA:
-        #     cx = cx.cuda()
-        #     cy = cy.cuda()
-        # cx, cy = torch.meshgrid(cx, cy)
-        # cx = cx.reshape(-1, 1)
-        # cy = cy.reshape(-1, 1)
-        # cxy = torch.cat((cx, cy), 1).repeat(1, 3).reshape(-1, 2).unsqueeze(0)
-        # x = x, cxy
-        # t1 = time.time()
-        # print('add cx and cy: {:.5f} sec'.format(t1-t0))
+            # normalize
+            t0 = time.time()
+            x[i][:, :, :4] *= (self.input_height // height)
+            t1 = time.time()
+            print('normalize: {:.5f} sec'.format(t1-t0))
+
+        tt1 = time.time()
+        print('transform: {:.5f} sec'.format(tt1-tt0))
+
+        # concatnate feature maps into single feature map
+        t0 = time.time()
+        x = torch.cat(x, 1)
+        t1 = time.time()
+        print('concatenate: {:.5f} sec'.format(t1-t0))
 
         return x
 
