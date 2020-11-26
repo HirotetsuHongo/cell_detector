f06e85d4a2e969009fdf172ab8875018e740170a
HirotetsuHongo <hongo.hirotetsu@j.mbox.nagoya-u.ac.jp>
2020-11-26 21:12:58 +0900

    Completed postprocess!!!

--- workspace/model/__init__.py
+++
@@ -1,1 +0,0 @@
-from model.head import Net
--- workspace/model/head.py
+++ workspace/model/head.py
@@ -1,4 +1,3 @@
-import time
 import torch
 import torch.nn as nn
 from . backbone import Darknet
@@ -67,143 +66,111 @@ class YOLOv3(nn.Module):
         return scale1, scale2, scale3
 
 
-class Net(nn.Module):
-    def __init__(self, input_height, input_width, num_filters, num_classes,
-                 anchors, confidency_threshold=0.5, iou_threshold=0.4,
-                 CUDA=True):
-        super(Net, self).__init__()
-        self.yolov3 = YOLOv3(num_filters, num_classes)
-        self.input_height = input_height
-        self.input_width = input_width
-        self.num_filters = num_filters
-        self.num_classes = num_classes
-        self.anchors = torch.Tensor(anchors)
-        self.confidency = confidency_threshold
-        self.iou = iou_threshold
-        self.CUDA = CUDA
-        if self.CUDA:
-            self.anchors = self.anchors.cuda()
+def postprocess(scales, anchors, input_height, input_width,
+                objectness_threshold, iou_threshold, CUDA=True):
+    def reshape(scale, batch_size, height, width, num_anchors, bbox_size):
+        scale = scale.transpose(1, 3)
+        scale = scale.reshape(batch_size,
+                              height * width * num_anchors,
+                              bbox_size)
+        return scale
 
-    def forward(self, x):
-        # apply YOLOv3
-        x = self.yolov3(x)
-        x = list(x)
+    def sigmoid_objectness(scale):
+        scale[:, :, 4] = torch.sigmoid(scale[:, :, 4])
+        return scale
 
-        # transform feature maps
-        for i in range(len(x)):
-            # define variables
-            batch_size = x[i].size(0)
-            height = x[i].size(2)
-            width = x[i].size(3)
-            num_anchors = 3
-            bbox_size = x[i].size(1) // num_anchors
+    def adjust_coordinate(scale, height, width, stride, num_anchors, anchors):
+        # sigmoid x and y
+        scale[:, :, 0:2] = torch.sigmoid(scale[:, :, 0:2])
 
-            # reshape the feature map
-            x[i] = x[i].transpose(1, 3)
-            x[i] = x[i].reshape(batch_size,
-                                height*width*num_anchors,
-                                bbox_size)
+        # add center coordinate of bbox
+        grid_x = torch.arange(width)
+        grid_y = torch.arange(height)
+        if CUDA:
+            grid_x = grid_x.cuda()
+            grid_y = grid_y.cuda()
+        center_x, center_y = torch.meshgrid(grid_x, grid_y)
+        center_x = center_x.unsqueeze(-1)
+        center_y = center_y.unsqueeze(-1)
+        center = torch.cat((center_x, center_y), -1)
+        center = center.repeat(1, num_anchors, 1).reshape(-1, 2)
+        scale[:, :, 0:2] += center
 
-            # apply sigmoid to the confidency
-            # and classes probability distribution
-            x[i][:, :, 4:] = torch.sigmoid(x[i][:, :, 4:])
+        # normalize x and y
+        scale[:, :, 0] *= stride[0]
+        scale[:, :, 1] *= stride[1]
 
-            # apply sigmoid to the tx and ty
-            x[i][:, :, 0] = torch.sigmoid(x[i][:, :, 0])
-            x[i][:, :, 1] = torch.sigmoid(x[i][:, :, 1])
+        # log scale transform w, h
+        scale[:, :, 2:4] = torch.exp(scale[:, :, 2:4])
+        # use anchors
+        anchors = [[a[0] / stride[0], a[1] / stride[1]] for a in anchors]
+        anchors = torch.tensor(anchors)
+        if CUDA:
+            anchors = anchors.cuda()
+        anchors = anchors.repeat(width * height, 1)
+        scale[:, :, 2:4] *= anchors
 
-            # add cx and cy to tx and ty
-            cx = torch.arange(width)
-            cy = torch.arange(height)
-            if self.CUDA:
-                cx = cx.cuda()
-                cy = cy.cuda()
-            cx, cy = torch.meshgrid(cx, cy)
-            cx = cx.unsqueeze(2)
-            cy = cy.unsqueeze(2)
-            cxy = torch.cat((cx, cy), 2)
-            cxy = cxy.repeat(1, 1, num_anchors)
-            cxy = cxy.reshape(-1, 2)
-            x[i][:, :, :2] += cxy
+        # normalize w and h
+        scale[:, :, 2] *= stride[0]
+        scale[:, :, 3] *= stride[1]
 
-            # log space transform height and width using anchors
-            anchors = self.anchors[i].repeat(height*width, 1).unsqueeze(0)
-            x[i][:, :, 2:4] = torch.exp(x[i][:, :, 2:4])
-            x[i][:, :, 2:4] *= anchors
+        return scale
 
-            # normalize
-            x[i][:, :, 0] *= (self.input_width // width)
-            x[i][:, :, 1] *= (self.input_height // height)
-            x[i][:, :, 2] *= (self.input_width // width)
-            x[i][:, :, 3] *= (self.input_height // height)
+    def low_objectness_suppress(image, objectness_threshold):
+        mask = image[:, 4] >= objectness_threshold
+        return image[mask, :]
 
-        # concatnate feature maps into single feature map
-        x = torch.cat(x, 1)
+    def select_class(image):
+        class_id = torch.max(image[:, 5:], 1)[1]
+        class_id = class_id.unsqueeze(1)
+        image = torch.cat((image[:, :5], class_id), 1)
+        return image
 
-        # split by batch size
-        x = [x[i] for i in range(x.size(0))]
+    def non_maximum_suppress(image, iou_threshold):
+        iou_mask = bbox_iou(image.unsqueeze(-1), image) > iou_threshold
+        conf_mask = image[:, 4].unsqueeze(-1) >= image[:, 4]
+        id_mask = image[:, 5].unsqueeze(-1) == image[:, 5]
+        mask = torch.logical_and(iou_mask, conf_mask)
+        mask = torch.logical_and(mask, id_mask)
+        mask = torch.any(mask, 1)
+        return image
 
-        for i in range(len(x)):
-            # threshold by confidency
-            conf_mask = x[i][:, 4] > self.confidency
-            x[i] = x[i][conf_mask, :]
+    def bbox_iou(bbox1, bbox2):
+        x1, y1, w1, h1 = bbox1[:, 0], bbox1[:, 1], bbox1[:, 2], bbox1[:, 3]
+        x2, y2, w2, h2 = bbox2[:, 0], bbox2[:, 1], bbox2[:, 2], bbox2[:, 3]
 
-            # convert bbox part
-            # from (center x, center y, width, height)
-            # to (xmin, ymin, xmax, ymax)
-            bbox = torch.empty(x[i].size(0), 4)
-            bbox[:, 0] = (x[i][:, 0] - x[i][:, 2]/2)
-            bbox[:, 1] = (x[i][:, 0] + x[i][:, 2]/2)
-            bbox[:, 2] = (x[i][:, 1] - x[i][:, 3]/2)
-            bbox[:, 3] = (x[i][:, 1] + x[i][:, 3]/2)
-            x[i][:, :4] = bbox
+        area1 = (w1 + 1) * (h1 + 1)
+        area2 = (w2 + 1) * (h2 + 1)
+        inter_w = torch.clamp(((w1 + w2) / 2) - torch.abs(x1 - x2), min=0)
+        inter_h = torch.clamp(((h1 + h2) / 2) - torch.abs(y1 - y2), min=0)
+        inter_area = (inter_w + 1) * (inter_h + 1)
+        union_area = area1 + area2 - inter_area
+        iou = inter_area / union_area
 
-            # select class
-            max_class_id = torch.max(x[i][:, 5:], 1)[1]
-            max_class_id = max_class_id.unsqueeze(1)
-            x[i] = torch.cat((x[i][:, :5], max_class_id), 1)
+        return iou
 
-            # non-maximum suppression
-            # bbox mask
-            nms_mask = torch.logical_and(
-                # bbox mask
-                bbox_iou(x[i].unsqueeze(-1), x[i]) > self.iou,
-                # id mask
-                x[i][:, 5].unsqueeze(-1) == x[i][:, 5])
-            nms_mask = torch.logical_and(
-                nms_mask,
-                # confidency mask
-                x[i][:, 4].unsqueeze(-1) < x[i][:, 4])
-            nms_mask = torch.any(nms_mask, 1)
-            x[i] = x[i][nms_mask, :]
+    scales = list(scales)
+    for i in range(len(scales)):
+        batch_size = scales[i].size(0)
+        height = scales[i].size(2)
+        width = scales[i].size(3)
+        num_anchors = len(anchors[i])
+        bbox_size = scales[i].size(1) // num_anchors
+        stride = (input_width / width, input_height / height)
 
-        # concatnate feature maps into single feature map
-        x = torch.cat(x, 0)
+        scales[i] = reshape(scales[i],
+                            batch_size, height, width, num_anchors, bbox_size)
+        scales[i] = sigmoid_objectness(scales[i])
+        scales[i] = adjust_coordinate(scales[i],
+                                      height, width, stride,
+                                      num_anchors, anchors[i])
 
-        return x
+    images = torch.cat(scales, 1)
+    images = [images[i] for i in range(images.size(0))]
 
+    for i in range(len(images)):
+        images[i] = low_objectness_suppress(images[i], objectness_threshold)
+        images[i] = select_class(images[i])
+        images[i] = non_maximum_suppress(images[i], iou_threshold)
 
-def bbox_iou(bbox1, bbox2):
-    """
-    Returns intersection over union of bbox1 and bbox2.
-    """
-    xmin1, ymin1 = bbox1[:, 0], bbox1[:, 1]
-    xmax1, ymax1 = bbox1[:, 2], bbox1[:, 3]
-    xmin2, ymin2 = bbox2[:, 0], bbox2[:, 1]
-    xmax2, ymax2 = bbox2[:, 2], bbox2[:, 3]
-    ixmin = torch.max(xmin1, xmin2)
-    iymin = torch.max(ymin1, ymin2)
-    ixmax = torch.min(xmax1, xmax2)
-    iymax = torch.min(ymax1, ymax2)
-
-    bbox1_area = \
-        torch.clamp(xmax1 - xmin1 + 1, 0) * torch.clamp(ymax1 - ymin1 + 1, 0)
-    bbox2_area = \
-        torch.clamp(xmax2 - xmin2 + 1, 0) * torch.clamp(ymax2 - ymin2 + 1, 0)
-    inter_area = \
-        torch.clamp(ixmax - ixmin + 1, 0) * torch.clamp(iymax - iymin + 1, 0)
-    union_area = bbox1_area + bbox2_area - inter_area
-
-    iou = inter_area / union_area
-
-    return iou
+    return images
