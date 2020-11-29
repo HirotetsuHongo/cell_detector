b9cf23e4405ca9a4bb184910150363cd4863e383
HirotetsuHongo <hongo.hirotetsu@j.mbox.nagoya-u.ac.jp>
2020-11-30 03:01:39 +0900

    Updated config.

--- workspace/config/config.py
+++ workspace/config/config.py
@@ -8,8 +8,5 @@ with open('/workspace/config/config.yaml') as yaml_file:
     config = yaml_file.read()
     config = yaml.load(config, Loader=yaml.SafeLoader)
 
-if config['CUDA'] == 'Enable':
+if config['CUDA']:
     assert torch.cuda.is_available() is True
-    config['device'] = torch.device('cuda')
-else:
-    config['device'] = torch.device('cpu')
