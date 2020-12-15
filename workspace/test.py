c85f1be1208301faf2bceeb8330efe977f5a7e7e
HirotetsuHongo <hongo.hirotetsu@j.mbox.nagoya-u.ac.jp>
2020-12-15 23:17:48 +0900

    Completed test.py.

--- workspace/test.py
+++ workspace/test.py
@@ -1,24 +1,70 @@
-import torch
 import model
+import config as cfg
+import preprocess as pre
+import postprocess as post
+
 import time
 
 
-# x = torch.randn(6, 1, 416, 416, requires_grad=True).cuda()
-# anchors = [[[10, 13], [16, 30], [33, 23]],
-#            [[30, 61], [62, 45], [59, 119]],
-#            [[116, 90], [156, 198], [373, 326]]]
-# f = model.Net(416, 416, 1, 4, anchors).cuda()
+def main():
+    # constants
+    num_channels = cfg.config['num_channels']
+    classes = cfg.config['classes']
+    num_classes = len(classes)
+    height = cfg.config['height']
+    width = cfg.config['width']
+    anchors = cfg.config['anchors']
+    cuda = cfg.config['CUDA']
+    target_dir = cfg.config['path']['test']
+    image_dir = cfg.config['path']['image']
+    weight_dir = cfg.config['path']['weight_test']
+    image_paths = pre.load_image_paths(image_dir, target_dir)
+    target_paths = pre.load_dir_paths(target_dir)
+    weight_paths = pre.load_dir_paths(weight_dir)
+    num_images = len(image_paths)
 
-x = torch.tensor([3., 2., 4.], requires_grad=True).cuda()
+    # network
+    net = model.YOLOv3(num_channels, num_classes)
+    if cuda:
+        net = net.cuda()
+    net = net.train(False)
 
-t0 = time.time()
-y = x * 10
-t1 = time.time()
-print("forward time: {:.3f} ms".format((t1-t0)*1000))
+    # calculate loss for each weights
+    for weight_path in weight_paths:
+        loss = 0.0
+        t0 = time.time()
 
-t0 = time.time()
-y.mean().backward()
-t1 = time.time()
-print("backward time: {:.3f} ms".format((t1-t0)*1000))
+        for i in range(num_images):
+            # load images and targets
+            image = pre.load_image(image_paths[i],
+                                   height,
+                                   width,
+                                   cuda)
+            image = image.unsqueeze(0)
+            target = pre.load_bbox(target_paths[i],
+                                   num_classes,
+                                   height,
+                                   width,
+                                   cuda)
 
-print("x.grad: {}".format(x.grad))
+            # predictions
+            predictions = net(image)
+
+            # calculate loss
+            loss_i = post.calculate_loss(predictions,
+                                         target,
+                                         anchors,
+                                         height,
+                                         width,
+                                         cuda)
+            loss += loss_i
+
+        loss /= num_images
+        elapsed_time = time.time() - t0
+
+        print("Weight: {}, Time: {:.3f}, Loss: {:.3f}"
+              .format(weight_path, elapsed_time, loss))
+
+
+if __name__ == '__main__':
+    main()
