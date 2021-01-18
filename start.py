7dc8d31d92aeccc5ff56204fc873422f14c222f4
HirotetsuHongo <hongo.hirotetsu@j.mbox.nagoya-u.ac.jp>
2021-01-19 04:22:24 +0900

    Fixed start.py.

--- start.py
+++ start.py
@@ -4,9 +4,10 @@ import os
 
 def main():
     # run container
-    status_exist = sp.Popen(['docker', 'ps', '-a'], stdout=sp.Pipe)
+    status_exist = sp.Popen(['docker', 'ps', '-a'], stdout=sp.PIPE)
     status_exist = sp.Popen(['grep', 'cell_detector'],
                             stdin=status_exist.stdout)
+    status_exist.wait()
     status_exist = status_exist is not None
     if not status_exist.stdout:
         sp.run('./commands/run')
@@ -15,6 +16,7 @@ def main():
     status_up = sp.Popen(['docker', 'ps'], stdout=sp.PIPE)
     status_up = sp.Popen(['grep', 'cell_detector'],
                          stdin=status_up.stdout)
+    status_up.wait()
     status_up = status_up is not None
     if not status_up:
         sp.run('./commands/start')
